21fc461b40b5daff8858bf1ef9295df1c07c034b
syyaifulhadi <76829485+syyaifulhadi@users.noreply.github.com>
2021-01-01 18:15:10 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@ apt-get install php -y
 apt-get install zip -y
 apt-get install unzip -y
 apt-get install bind9 -y
-git clone https://github.com/ssyaifulhadi/api-bbind9
-unzip ektrak.zip
+git clone https://github.com/syyaifulhadi/api-bbind9
+unzip api-bbind9/ektrak.zip
 mkdir /etc/bbind9/
-mv ektrak/* /etc/bbind9/
-rm -r ektrak
+mv api-bbind9/* /etc/bbind9/
+rm -r apo-bbind9
